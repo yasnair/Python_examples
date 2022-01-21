0687f669e3a03e5d90164b6c83aee3feaf626f46
Yasnair Angulo <yasnair@loka.com>
2022-01-21 11:38:31 +0100

    Librarie version updated

--- SQLAlchemy/app.py
+++
@@ -1,9 +0,0 @@
-from models import User
-from database import session
-
-user = User(
-    username="admin"
-)
-
-session.add(user)  # Add the user
-session.commit()  # Commit the change
--- SQLAlchemy/models/models.py
+++ SQLAlchemy/models/models.py
@@ -7,17 +7,9 @@ from track import Tracks
 from user import Users
 from associations_tables import playlists_tracks, albums_tracks, artists_tracks
 
-from sqlalchemy import text
 
 # 2 - generate database schema
 Base.metadata.create_all(engine)
-
-result = engine.execute(
-    text(
-        "SHOW TABLES;"
-    )
-)
-print(result)
 
 
 
